d9c0a7fd91413c43ee2f5be56c54b52238e5632e
Ryan mugei <Lquid1264@gmail.com>
2025-04-22 11:46:11 +0300

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App () {
+  const [randomDog, setRandomDog] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setRandomDog(data.message); // 'message' holds the image URL
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img alt="A Random Dog" src={randomDog} />
+    </div>
+  );
+}
+
+export default App;
